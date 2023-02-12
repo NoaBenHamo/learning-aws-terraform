e5dccd5b253d4e81daad54f72fc96f35a3d9b6ed
Ubuntu <ubuntu@ip-172-31-40-66.eu-west-1.compute.internal>
2023-02-12 13:03:19 +0000

    modified main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
-resource "aws_security_group" "allow-ssh" {
-name = "allow-ssh"
+resource "aws_security_group" "allow-ssh-sg" {
+name = "allow-ssh-sg"
 ingress {
     cidr_blocks = [
       "0.0.0.0/0"
@@ -28,8 +28,8 @@ from_port = 22
 resource "aws_instance" "app_server" {
   ami           = "ami-0cc4e06e6e710cd94"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow-ssh-sg.id]
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "ExampleInstance"
   }
 }
